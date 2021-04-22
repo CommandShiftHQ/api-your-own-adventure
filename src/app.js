34cfdd527653fc2d7d338c967192b1e3add386cd
ian-antking <ian.antking@gmail.com>
2021-04-22 17:54:17 +0100

    add express.static and cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
+const cors = require('cors');
 const swagger = require('../swagger.json');
 const locationRouter = require('./routes/location');
 
@@ -7,8 +8,16 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cors());
+
+app.use(express.static('public'));
+
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swagger));
 
 app.use('/location', locationRouter);
+
+app.get('*', (_, res) => {
+  res.redirect('/');
+});
 
 module.exports = app;
