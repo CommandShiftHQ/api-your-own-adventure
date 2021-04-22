fecf8d6953164aaaa8b57677555271addb47e006
ian-antking <ian.antking@gmail.com>
2021-04-22 13:42:31 +0100

    list routes when app starts

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const expressListRoutes = require('express-list-routes');
 const app = require('./src/app');
 const logger = require('./src/utils/logger');
 
 const APP_PORT = process.env.PORT || 3000;
 
-app.listen(APP_PORT, () => logger.info(`app is listening on localhost:${APP_PORT}`));
+app.listen(APP_PORT, () => {
+  expressListRoutes(app);
+  logger.info(`app is listening on localhost:${APP_PORT}`);
+});
